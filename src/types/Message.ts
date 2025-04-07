2627b900bd156e103a866134f0c67ab8861e450a
Abozaid Blacklane <abozaid@lenaai.net>
2025-04-07 03:11:18 +0200

    remove duplicate file from messages

--- src/types/Message.ts
+++ src/types/Message.ts
@@ -1,9 +1,10 @@
-interface Message {
+
+export default interface Message {
   id: number
   type: 'text' | 'voice' | 'imageAlbum'
   content: string | { url: string; full: string }[]
   sender: 'user' | 'server'
   duration?: string
-}
+} 
 
-export default Message 
+
